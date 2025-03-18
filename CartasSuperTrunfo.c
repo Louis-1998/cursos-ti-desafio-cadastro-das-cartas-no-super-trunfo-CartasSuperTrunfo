4b55efe567df5f473b66c1f9d6f2fe2dab0e13ba
Louis-1998 <luiz.anderson2503@gmail.com>
2025-03-17 22:23:10 -0300

    Atualizar o CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,9 +4,10 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//Teste Anderson 
 
 int main() {
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
